9a2230574e19baa919f19d0564915e527490259e
Arun Verma <info.arunverma23@gmail.com>
2025-07-28 17:48:53 +0000

    message

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,9 +1,9 @@
-let score = "33abc";
+// let score = "33abc";
 
 // console.log(typeof score) // number
 // console.log(typeof (score))
 
-let value_number = Number(score)
+// let value_number = Number(score)
 
 // console.log(typeof value_number) // number
 // console.log(value_number) //=> NaN : not a number
@@ -12,20 +12,76 @@ let value_number = Number(score)
 // "33abc" =? not easily converted to number
 // true => 1 false => 0
 
-let isLoggedIn = 1
+// let isLoggedIn = 1
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
 
-console.log(booleanIsLoggedIn) // true
+// console.log(booleanIsLoggedIn) // true
 
 
-var no = 3;
+// var no = 3;
 
-console.log(typeof no)
+// console.log(typeof no)
 
-var no = String(no)
+// var no = String(no)
 
-console.log(typeof no)
+// console.log(typeof no)
+
+// let value = 3;
+// let neg_value = -value
+// console.log(neg_value)
+
+
+// console.log(2+2)
+// console.log(2^1)
+// console.log(2-2)
+// console.log(2/2)
+// console.log(2*2)
+// console.log(2%3)
+
+
+// let str1 = "arun"
+// let str2 = "verma "
+
+// console.log(str1+str2)
+
+
+// console.log("1"+"2")
+// console.log("1"+ 2)
+// console.log("1"+2+2)
+// console.log(1+1+"2")
+
+// console.log(3+4*5%3)
+
+
+// console.log(+true)
+// console.log(+"")
+
+
+// let num1 , num2 , num3 = 4
+
+// num1 = num2 = num3 = 2+2
+
+// console.log(num1)
+
+
+
+// let gamecounter = 100
+
+// gamecounter++
+
+// console.log(gamecounter++)
+
+// ++gamecounter
+
+// console.log(gamecounter)
+
+
+
+
+
+
+
 
 
 
