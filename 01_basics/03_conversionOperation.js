f3359524e1c59817e90233a4f1fcff085ac1dea7
Arun Verma <info.arunverma23@gmail.com>
2025-07-28 01:48:17 +0000

    Add conversion operation file

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,33 @@
+let score = "33abc";
+
+// console.log(typeof score) // number
+// console.log(typeof (score))
+
+let value_number = Number(score)
+
+// console.log(typeof value_number) // number
+// console.log(value_number) //=> NaN : not a number
+
+// "33" =? easily converted to number
+// "33abc" =? not easily converted to number
+// true => 1 false => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn) // true
+
+
+var no = 3;
+
+console.log(typeof no)
+
+var no = String(no)
+
+console.log(typeof no)
+
+
+
+
+
