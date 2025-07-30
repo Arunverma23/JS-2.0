5c7dbcf314e5640ce151d3139eadf4e536eab41b
Arun Verma <info.arunverma23@gmail.com>
2025-07-30 23:44:28 +0000

    message

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,7 @@
+const names1 = ["arun" , "harsh" , "ichcha" , "utkarsh"]
+const names2 = ["arun" , "sneha" , "jagrit" , "manjeet"]
+
+names1.push(names2)
+
+console.log(names1)
+console.log(names1[4][1])
