6e4fbab6570bdbcb191a50af07adffa8089f49b8
Arun Verma <info.arunverma23@gmail.com>
2025-07-30 23:58:52 +0000

    message

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -1,7 +1,35 @@
 const names1 = ["arun" , "harsh" , "ichcha" , "utkarsh"]
 const names2 = ["arun" , "sneha" , "jagrit" , "manjeet"]
 
-names1.push(names2)
+// names1.push(names2)
 
-console.log(names1)
-console.log(names1[4][1])
+// console.log(names1)
+// console.log(names1[4][1])
+
+// const name = names1.concat(names2)
+// console.log(name)
+
+// better way
+
+// const name = [...names1,...names2]
+// console.log(name)
+
+// const arr = [1,2,3,[4,5,6],7,[6,7,[4,5]]]
+// const new_arr = arr.flat(Infinity)
+// console.log(arr)
+// console.log(new_arr)
+
+// console.log(Array.isArray("arun"))
+// console.log(Array.from("Arun"))
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1,score2,score3))
+
+
+
+
+
+
