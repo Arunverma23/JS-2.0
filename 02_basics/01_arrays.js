929e8b9d4fb7e0dcc5a182e888440c7ba44bb067
Arun Verma <info.arunverma23@gmail.com>
2025-07-30 23:33:45 +0000

    message

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,42 @@
+// array
+
+const myArr = [0 ,1 ,2, 3, 4, 5]
+const my_name = ["arun" , "verma"]
+const my_name2 = new Array(1,2,3,4)
+
+// console.log(myArr[0])
+
+// Array methods
+
+// console.log(myArr.length)
+// myArr.push(6)
+// myArr.pop()
+// myArr.push(8)
+
+// myArr.unshift(9)
+// myArr.shift()
+
+// console.log(myArr.includes(9))
+// console.log(myArr.indexOf(2))
+// console.log(myArr.at(1))
+
+// const new_arr = myArr.join()
+// console.log(typeof(myArr))
+// console.log(typeof(new_arr))
+
+// slice , 
+
+console.log("A", myArr)
+
+const my_new_arr_1 = myArr.slice(1,3)
+
+console.log(my_new_arr_1)
+console.log("b" , myArr)
+
+const my_new_arr_2 = myArr.splice(1,3)
+console.log(myArr)
+console.log("c" , my_new_arr_2)
+
+
+
+
