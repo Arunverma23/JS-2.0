26ad162ae6db954cb1ccbb8bbf6266dea1b9da70
Arun Verma <info.arunverma23@gmail.com>
2025-07-31 13:39:52 +0000

    message

---
+++ 04_control_flow/one.js
@@ -0,0 +1,30 @@
+// if 
+
+// if (true){ // condition must be true
+
+// }
+
+// if(false){ // no execution
+
+// }
+
+// const is_user_logged_in = true
+// if(is_user_logged_in){ // comparison
+
+// }
+
+// < , > , <= , >= , == , != , === (strictly equal (also check type ))
+
+// if(2==="2"){
+//     console.log("executed")
+// }
+// else if(2=="2"){
+//      console.log("executed")
+// }
+// else{
+//     console.log("not executed")
+// }
+
+if(2!=2) console.log("h");
+
+else if(2==2) console.log("hh") ;   
---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,41 @@
+const email = ""
+
+// falsy value : false , 0 , -0 , bigint 0n ,"" , NaN , undefined , null
+
+// truthy value : "0",'false' , " " ,[] , {} , function(){}
+
+// if(email){
+//     console.log("got email")
+// }
+// else{
+//     console.log("dont have email")
+// }
+
+// const arr = []
+// if(arr.length === 0){
+//     console.log("array is empty")
+// }
+
+// const obj = {}
+// if(Object.keys(obj).length === 0)
+// {
+//     console.log("empty")
+// }
+
+// nullish coalescing operator (??): null , undefined
+
+// let val1
+// val1 = 5??10
+// val1 = null??10
+// val1 = undefined ?? 10
+// val1 = null ?? null ?? 8
+
+// console.log(val1)
+
+// ternary operator 
+
+// consdition ? true : false
+
+// const price = 200
+
+// price == 201 ? console.log("200") : console.log("201")
