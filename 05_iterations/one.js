8f8c5b9a913abbe9cd1d62529b9bc225af00f10b
Arun Verma <info.arunverma23@gmail.com>
2025-07-31 17:10:18 +0000

    message

---
+++ 05_iterations/one.js
@@ -0,0 +1,26 @@
+// for
+
+// const Array = [1,2,3,4,5,6,7,8,9,10]
+// for(let index = 0; index<Array.length;index++)
+// {
+//     console.log(Array[index])
+// }
+
+const matrix = [[1,2,3],[4,5,6],[7,8,9]]
+// for(let i = 0;i<matrix.length;i++)
+// {
+//     for(let j=0;j<matrix[i].length;j++)
+//     {
+//         // console.log(matrix[i][j]) // always add a new line
+//         process.stdout.write(" "+matrix[i][j]) // without adding newlines
+//     }
+//     console.log()
+// }
+
+// for(let i=0;i<11;i++)
+// {
+//     console.log(i*2)
+// }
+
+// break
+// contniue
