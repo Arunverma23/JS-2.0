6ef8d690c5608015e236392bf62edbab7b956fa1
Arun Verma <info.arunverma23@gmail.com>
2025-08-01 13:52:51 +0000

    message

---
+++ 05_iterations/two.js
@@ -0,0 +1,33 @@
+// while loop
+
+// while(condition){
+
+// }
+
+let i=0;
+// while(i<11)
+// {
+//     console.log(i)
+//     i++
+// }
+
+// const arr = [1,2,3,4,5,6,7,8,9,10]
+// i=11;
+// while(i<=100)
+// {
+//     arr.push(i)
+//     i++
+// }
+// i=0
+// while(i<100)
+// {
+//    console.log(`value of array ${arr[i]} at index ${arr[i]-1}`)
+//    i++
+// }
+
+let index = 11
+do {
+    console.log(index)
+    index++
+
+} while(index<10)
