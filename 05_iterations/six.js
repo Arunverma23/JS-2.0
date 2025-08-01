ffedefe756c20cd1e9e39dbc307e797deca55dcd
Arun Verma <info.arunverma23@gmail.com>
2025-08-01 23:18:39 +0000

    message

--- 05_iterations/six.js
+++ 05_iterations/six.js
@@ -17,13 +17,33 @@ const nums = [1,2,3,4,5,6,7,8,9,10]
 //     return num>4
 // })
 
-const new_nums = []
+// const new_nums = []
 
-nums.forEach((num)=>{
-    if(num>4)
-    {
-        new_nums.push(num)
-    }
-})
+// nums.forEach((num)=>{
+//     if(num>4)
+//     {
+//         new_nums.push(num)
+//     }
+// })
 
-console.log(new_nums)
+// console.log(new_nums)
+
+const books = [
+    { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
+    { title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008 },
+    { title: 'Book Three', genre: 'History', publish: 1999, edition: 2007 },
+    { title: 'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010 },
+    { title: 'Book Five', genre: 'Science', publish: 2009, edition: 2014 },
+    { title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010 },
+    { title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996 },
+    { title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016 },
+    { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
+  ];
+
+  let user_books = books.filter((bk)=> bk.genre === 'History')
+
+  user_books = books.filter((bk)=> bk.publish >= 1990 && bk.genre === 'History')
+
+  
+
+  console.log(user_books)
