68f49b5bdcb9761d31d6e61e81f5fca575aaec09
Arun Verma <info.arunverma23@gmail.com>
2025-08-24 09:03:45 +0000

    message

---
+++ 01_basics/07_datesinJs.js
@@ -0,0 +1,34 @@
+// dates
+
+// let myDate = new Date()
+// console.log(myDate)
+// console.log(myDate.toString());
+// console.log(myDate.toJSON())
+// console.log(myDate.toLocaleString())
+// console.log(myDate.toDateString())
+// console.log(typeof myDate)
+
+// let myCreatedDate = new Date(2023, 0, 23, 5)
+// let myCreatedDate = new Date(2023, 0, 23, 5, 3, 6)
+// let myCreatedDate = new Date("2025-08-24")
+// let myCreatedDate = new Date("08-24-2025")
+
+// console.log(myCreatedDate)
+// console.log(myCreatedDate.toDateString())
+// console.log(myCreatedDate.toLocaleString())
+
+// let my_time_stamp = Date.now()
+
+// console.log(my_time_stamp)
+// console.log(myCreatedDate.getTime())
+// console.log(Math.floor(Date.now()/1000))
+
+// let New_date = new Date()
+// console.log(New_date)
+// console.log(New_date.getFullYear())
+
+// New_date.toLocaleString('default', {
+//     weekday:"long",
+//     time_zone:'timezone'
+// })
+
